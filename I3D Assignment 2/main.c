7b8f1c7f2b44d82960500744d07e7ada42c97398
Karim Ainine <karim.ainine@gmail.com>
2012-05-16 13:07:28 +1000

    Initial Commit

---
+++ I3D Assignment 2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  I3D Assignment 2
+//
+//  Created by Karim Ainine on 16/05/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+   // insert code here...
+   printf("Hello, World!\n");
+    return 0;
+}
+

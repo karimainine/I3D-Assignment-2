5b8d83103dd17d84982e2c4b7e847d1b0b16cf63
Karim Ainine <karim.ainine@gmail.com>
2012-05-24 02:03:44 +1000

    a stupid commit

--- I3D Assignment 2/seabed.h
+++ I3D Assignment 2/seabed.h
@@ -6,9 +6,38 @@ extern "C" {
 #endif
 	
 #include "utils.h"
-
-void drawSeaBed();
-
+	
+	/* The Grid struct is used to hold the grid of vertices representing
+	 the waves */
+	typedef struct
+	{
+		int rows;		/* No. of vertices per row (tessellation) */
+		int cols;		/* No. of vertices per col (tessellation) */
+		float size;		/* Size of the grid in GL coords (width and height are equal) */
+		int nVertices;		/* Total no. of vertices */
+		int nIndices;		/* Total no. of indices */
+		Vec3f *vertices;	/* 1d array of vertices */
+		Vec3f *normals;		/* 1d array of normal vectors, maps to
+									 locations of vertices */
+		int *indices;		/* 1d array of indices */
+	} Terrain;
+	
+	/* Initialises a 2d grid of the given size, divided into the given
+	 number of rows and cols */
+	void initTerrain(Terrain *terrain, int rows, int cols, float size, float height_offset);
+	
+	void calcTerrainHeights(Terrain *terrain, float height_offset);
+	
+	/* Deletes all memory dynamically allocated by initGrid */
+	void cleanupTerrain(Terrain *terrain);
+			
+	/* Draws a given grid */
+	void drawTerrain(Terrain *terrain);
+	
+	/* Draws normal vectors of the grid as lines, for debugging
+	 purposes */
+	void drawTerrainNormals(Terrain *terrain, float size);
+	
 #ifdef __cplusplus
 }
 #endif
